6ba3618649cb0df734a5bf44cda28ec5574edbea
Enrique Llorente <ellorent@redhat.com>
2024-07-04 16:40:10 +0200

    udn: Add FindPrimaryNetwork helper
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

---
+++ pkg/udn/primary_network.go
@@ -0,0 +1,33 @@
+package udn
+
+import (
+	"context"
+	"fmt"
+
+	v1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/kubevirt/ipam-extensions/pkg/config"
+)
+
+func FindPrimaryNetwork(ctx context.Context,
+	cli client.Client,
+	namespace string) (*v1.NetworkAttachmentDefinition, error) {
+	nadList := v1.NetworkAttachmentDefinitionList{}
+	if err := cli.List(ctx, &nadList, client.InNamespace(namespace)); err != nil {
+		return nil, fmt.Errorf("failed listing nads for pod namespace %q", namespace)
+	}
+
+	for _, nad := range nadList.Items {
+		netConfig, err := config.NewConfig(nad.Spec.Config)
+		if err != nil {
+			return nil, fmt.Errorf("failed to extract the relevant NAD information")
+		}
+		if netConfig.Role == config.NetworkRolePrimary {
+			return ptr.To(nad), nil
+		}
+	}
+	return nil, nil
+}
