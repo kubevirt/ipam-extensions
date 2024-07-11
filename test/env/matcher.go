2007ee7d65a69824d6813f91b4c93f252cb38b47
Or Shoval <oshoval@redhat.com>
2024-07-11 15:51:27 +0300

    e2e: Fix IPAMClaimsFromNamespace
    Function should filter according namespace.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- test/env/matcher.go
+++ test/env/matcher.go
@@ -12,6 +12,8 @@ import (
 
 	kubevirtv1 "kubevirt.io/api/core/v1"
 
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	ipamclaimsv1alpha1 "github.com/k8snetworkplumbingwg/ipamclaims/pkg/crd/ipamclaims/v1alpha1"
 )
 
@@ -19,7 +21,7 @@ import (
 func IPAMClaimsFromNamespace(namespace string) func() ([]ipamclaimsv1alpha1.IPAMClaim, error) {
 	return func() ([]ipamclaimsv1alpha1.IPAMClaim, error) {
 		ipamClaimList := &ipamclaimsv1alpha1.IPAMClaimList{}
-		if err := Client.List(context.Background(), ipamClaimList); err != nil {
+		if err := Client.List(context.Background(), ipamClaimList, client.InNamespace(namespace)); err != nil {
 			return nil, err
 		}
 		return ipamClaimList.Items, nil
